d4a1e4559d940986801e91cc10953dfa8744262b
akashvishal2405 <akashvishal2405@gmail.com>
2024-06-28 13:36:17 +0530

    add changed files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from '../assets/kevinRushLogo.png';
-import { FaLinkedin } from 'react-icons/fa';
-import { FaGithub } from 'react-icons/fa';
-import { FaSquareXTwitter } from 'react-icons/fa6';
-import { FaInstagram } from 'react-icons/fa';
+import React from "react";
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className=' mb-20 flex items-center justify-between py-6'>
-    <div className='flex flex-shrink-0 items-center'>
-        <img className='mx-2 w-10' src={logo} alt="logo" />
-    </div>
-    <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <a href="https://www.linkedin.com/in/akash-s-9a3376315/" target='_blank'><FaLinkedin /></a>
-        <a href="https://github.com/akashvishal2405/" target='_blank'><FaGithub /></a>
-        <a href="" target='_blank'><FaSquareXTwitter /></a>
-        <a href="https://www.instagram.com/akash.pt_/" target='_blank'><FaInstagram /></a>
-    </div>
+    <nav className=" mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/akash-s-9a3376315/"
+          target="_blank"
+        >
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/akashvishal2405/" target="_blank">
+          <FaGithub />
+        </a>
+        <a href="" target="_blank">
+          <FaSquareXTwitter />
+        </a>
+        <a href="https://www.instagram.com/akash.pt_/" target="_blank">
+          <FaInstagram />
+        </a>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
