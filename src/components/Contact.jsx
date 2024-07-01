b70e73ec9bce974786a1c43be9b2a2fe5bb7f960
Akash <138090366+akashvishal2405@users.noreply.github.com>
2024-07-01 16:58:36 +0530

    Update Contact.jsx
    Added phone and e-mail text

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {CONTACT} from '../constants/index';
 import { motion } from "framer-motion";
-import { FaPhoneAlt } from "react-icons/fa";
+
 const Contact = () => {
 
   return (
@@ -16,8 +16,8 @@ const Contact = () => {
         initial={{opacity: 0, x: -100}}
         transition={{duration:1.5}}
         className="text-center tracking-tighter">
-         <FaPhoneAlt /> <p className='my-4'>{CONTACT.phoneNo}</p>
-            <a href="#" className='border-b'>{CONTACT.email}</a>
+         <p className='my-4'>{Phone: CONTACT.phoneNo}</p>
+            <a href="#" className='border-b'>{E-mail: CONTACT.email}</a>
         </motion.div>
     </div>
   )
